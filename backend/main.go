cf0b099457496c7173c00beda94e7dddd5cd3a93
hruday222 <ahruday222@gmail.com>
2024-06-21 15:34:45 +0530

    Updated backend files

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/hruday222/go-url-shortener/handler"
 	"github.com/hruday222/go-url-shortener/store"
@@ -10,6 +11,15 @@ import (
 
 func main() {
 	r := gin.Default()
+
+	// CORS middleware configuration
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"} // Add other origins as needed
+	config.AllowMethods = []string{"GET", "POST", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type"}
+
+	r.Use(cors.New(config))
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Welcome to the  URL Shortener API !🚀",
